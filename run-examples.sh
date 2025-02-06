acd1830c53fdb4e0ef0d133bff917db139d7bffc
eabalestra <agusbalestra0@gmail.com>
2025-02-05 21:28:34 -0300

    add getMin example

--- run-examples.sh
+++ run-examples.sh
@@ -9,3 +9,5 @@ echo "=> Example 2: Sorted List: insert(int data)"
 python search-counterexample.py examples/DataStructuresList_insert/List.java examples/DataStructuresList_insert/slist-specs.csv insert
 echo "=> Example 3: StackAr: topAndPop()"
 python search-counterexample.py examples/StackAr_topAndPop/StackAr.java examples/StackAr_topAndPop/stack-specs.csv topAndPop
+echo "=> Example 4: SimpleMethods: getMin()"
+python search-counterexample.py examples/simple-examples_getMin/SimpleMethods.java examples/simple-examples_getMin/simple-methods-specs.csv getMin
