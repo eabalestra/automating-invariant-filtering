05ed4a762a3029fcbf67465aa39af6bb8cab171b
eabalestra <agusbalestra0@gmail.com>
2025-02-07 11:50:03 -0300

    refactor test_assertion_inserter for replace all occurrences of asserts and add final assert

--- scripts/test_assertion_inserter.py
+++ scripts/test_assertion_inserter.py
@@ -1,15 +1,19 @@
 import re
 
 def insert_assertion_into_test_code(test_code, specification):
-    # extract the assertion condition from the specification
+    # generate the assertion from the specification
     assertion_statement = generate_assertion_from_specification(specification)
+    
     # define a pattern to match any assertion
-    assertion_pattern = re.compile(r'assert\w*\((?:[^)(]+|\((?:[^)(]+|\([^)(]*\))*\))*\);')
-    # replace all existing assertions with the new assertion
-    test_code = re.sub(assertion_pattern, assertion_statement, test_code, count=1)
+    assertion_pattern = re.compile(r'(?i)assert\w*\s*\(.*?\);', re.DOTALL)
+    
+    # replace ALL existing assertions with the new assertion
+    test_code, num_subs = re.subn(assertion_pattern, assertion_statement, test_code)
 
-    if not re.search(assertion_pattern, test_code):
-        test_code = re.sub(r'(\s*}\s*)$', f'\n    {assertion_statement}\n\\1', test_code)
+    if num_subs == 0:
+        end_of_method = re.compile(r'(\s*}\s*)', re.DOTALL)
+        assertion = f'    {assertion_statement}\\1'
+        test_code = re.sub(end_of_method, assertion, test_code)
     return test_code
 
 def generate_assertion_from_specification(specification):
@@ -22,4 +26,4 @@ def generate_assertion_from_specification(specification):
     elif 'xor' in specification:
         parts = re.split(r'\s*xor\s*', specification)
         specification = f'(!({parts[0]} && {parts[1]}) && ({parts[0]} || {parts[1]}))'
-    return 'assertTrue(' + specification + ');'
+    return '\n\n    assertTrue(' + specification + ');'
