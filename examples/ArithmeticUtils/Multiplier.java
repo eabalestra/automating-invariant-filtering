d8d2c2cdb696f8c47764970afe5244aacf0dab7c
eabalestra <agusbalestra0@gmail.com>
2025-02-12 18:58:14 -0300

    Remove Multiplier class and associated specifications; add SimpleMethods class with abs method and update example script

--- examples/ArithmeticUtils/Multiplier.java
+++
@@ -1,7 +0,0 @@
-package ArithmeticUtils;
-
-public class Multiplier {
-    public int multiply(int x, int y) {
-        return x * y;
-    }
-}
--- run-examples.sh
+++ run-examples.sh
@@ -3,11 +3,11 @@ if [ -z "$VIRTUAL_ENV" ]; then
     echo "Virtual environment not activated. Exiting."
     exit 1
 fi
-echo "=> Example 1: Multiplication: multiply(int a, int b)"
-python search-counterexample.py examples/ArithmeticUtils/Multiplier.java examples/ArithmeticUtils/multiply_specs.csv multiply
-echo "=> Example 2: Sorted List: insert(int data)"
+echo "=> Example 1: Sorted List: insert(int data)"
 python search-counterexample.py examples/DataStructuresList_insert/List.java examples/DataStructuresList_insert/slist-specs.csv insert
-echo "=> Example 3: StackAr: topAndPop()"
-python search-counterexample.py examples/StackAr_topAndPop/StackAr.java examples/StackAr_topAndPop/stack-specs.csv topAndPop
-echo "=> Example 4: SimpleMethods: getMin()"
+echo "=> Example 2: StackAr_topAndPop"
+python search-counterexample.py examples/simple-examples_abs/SimpleMethods.java examples/simple-examples_abs/SimpleMethods-abs-specs.csv abs
+echo "=> Example 3: simple-examples_getMin"
 python search-counterexample.py examples/simple-examples_getMin/SimpleMethods.java examples/simple-examples_getMin/simple-methods-specs.csv getMin
+# echo "=> Example 4: "
+# python search-counterexample.py 
