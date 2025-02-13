d6ee9f4fe106e30dd4d6b435b3cdd63847a1bb0f
eabalestra <agusbalestra0@gmail.com>
2025-02-13 20:27:42 -0300

    Add script for automatic invariant filtering and update example runner

--- run-examples.sh
+++ run-examples.sh
@@ -3,11 +3,12 @@ if [ -z "$VIRTUAL_ENV" ]; then
     echo "Virtual environment not activated. Exiting."
     exit 1
 fi
-echo "=> Example 1: Sorted List: insert(int data)"
-python search-counterexample.py examples/DataStructuresList_insert/List.java examples/DataStructuresList_insert/slist-specs.csv insert
-echo "=> Example 2: StackAr_topAndPop"
-python search-counterexample.py examples/StackAr_topAndPop/StackAr.java examples/StackAr_topAndPop/stack-specs.csv topAndPop
-echo "=> Example 3: simple-examples_getMin"
-python search-counterexample.py examples/simple-examples_getMin/SimpleMethods.java examples/simple-examples_getMin/simple-methods-specs.csv getMin
-# echo "=> Example 4: "
-# python search-counterexample.py 
+
+echo "==> Running example 1: getMin"
+
+for specs in examples/simple-examples_getMin/specs/*; do
+    echo "==> Running example $specs"
+    sh run-automatic-invariant-filtering.sh examples/simple-examples_getMin/src/main/java/examples/SimpleMethods.java $specs getMin examples/simple-examples_getMin/src/test/java/testers/SimpleMethodsTester0.java
+    mv examples/simple-examples_getMin/src/test/java/testers/SimpleMethodsTester0Augmented.java ../output/test/SimpleMethodsTester0Augmented${i}.java
+    i=$((i+1))
+done
