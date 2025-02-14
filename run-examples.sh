5b2b6318d98c90990c5112eb1aad3f1c62814879
eabalestra <agusbalestra0@gmail.com>
2025-02-14 11:44:38 -0300

    enhance run-examples.sh

--- run-examples.sh
+++ run-examples.sh
@@ -6,9 +6,15 @@ fi
 
 echo "==> Running example 1: getMin"
 
+i=0
 for specs in examples/simple-examples_getMin/specs/*; do
     echo "==> Running example $specs"
     sh run-automatic-invariant-filtering.sh examples/simple-examples_getMin/src/main/java/examples/SimpleMethods.java $specs getMin examples/simple-examples_getMin/src/test/java/testers/SimpleMethodsTester0.java
-    mv examples/simple-examples_getMin/src/test/java/testers/SimpleMethodsTester0Augmented.java ../output/test/SimpleMethodsTester0Augmented${i}.java
-    i=$((i+1))
+    augmented_file="examples/simple-examples_getMin/src/test/java/testers/SimpleMethodsTester0Augmented.java"
+    if [ -f "$augmented_file" ]; then
+        mv "$augmented_file" "output/test/SimpleMethods/SimpleMethodsTester0Augmented-${i}.java"
+        i=$((i+1))
+    else
+        echo "File $augmented_file does not exist. Skipping."
+    fi
 done
