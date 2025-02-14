d1fbdfdfc59f89e082ea81920509524f50b0dfd5
eabalestra <agusbalestra0@gmail.com>
2025-02-14 16:30:11 -0300

    Refactor test_extractor.py to enhance test extraction logic and improve comment handling

--- scripts/test_extractor.py
+++ scripts/test_extractor.py
@@ -1,24 +1,52 @@
 import re
+from typing import List, Tuple
 
-def extract_test(response_test):
-    lines = response_test.split('\n')
+def extract_test_with_comments_from_string(text: str) -> str:
+    lines = text.split('\n')
+    comments, extracted_test = parse_comments_and_test(lines)
+    return '\n'.join(comments + extracted_test)
+
+def extract_tests_from_file(source_test_file: str) -> List[str]:
+    with open(source_test_file, "r", encoding='utf-8') as sf:
+        content = sf.read()
+    return parse_test_from_string(content)
+
+def parse_comments_and_test(lines: List[str]) -> Tuple[List[str], List[str]]:
     test_case_started = False
     extracted_test = []
     comments = []
     brace_count = 0
-
     test_start_pattern = re.compile(r'^\s*@Test')
-    brace_pattern = re.compile(r'[{}]')
 
     for line in lines:
         if test_start_pattern.match(line):
             test_case_started = True
         if test_case_started:
-            extracted_test.append(line)
+            extracted_test.append("    " + line)
             brace_count += line.count('{') - line.count('}')
             if brace_count == 0 and line.strip().endswith('}'):
                 extracted_test.append("\n")
                 test_case_started = False
         else:
             comments.append(f"// {line}")
-    return '\n'.join(comments + extracted_test)
+    return comments, extracted_test
+
+def parse_test_from_string(content: str) -> List[str]:
+    brace_count = 0
+    test_methods = []
+    extracted_test = []
+    test_case_started = False
+    lines = content.split('\n')
+    test_start_pattern = re.compile(r'^\s*@Test')
+
+    for line in lines:
+        if test_start_pattern.match(line):
+            test_case_started = True
+        if test_case_started:
+            extracted_test.append("    " + line)
+            brace_count += line.count('{') - line.count('}')
+            if brace_count == 0 and line.strip().endswith('}'):
+                test_case_started = False
+                test_methods.append('\n'.join(extracted_test))
+                extracted_test = []
+    return test_methods
