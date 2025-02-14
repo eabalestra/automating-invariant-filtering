4be295d11fb645c0a5e173b9a0410ae9169a266c
eabalestra <agusbalestra0@gmail.com>
2025-02-14 11:44:58 -0300

    Refactor run-automatic-invariant-filtering.sh to improve parameter naming and enhance clarity in test generation process

--- run-automatic-invariant-filtering.sh
+++ run-automatic-invariant-filtering.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # parameters
-CLS=$1
-CSV_FILE=$2
+SUBJECT_CLASS_PATH=$1
+SPECS_FILE_PATH=$2
 METHOD_NAME=$3
-EXISTING_TEST_FILE=$4
+EXISTING_TEST_FILE_PATH=$4
 
 # copy the existing test suite
-cp "$EXISTING_TEST_FILE" "${EXISTING_TEST_FILE%.java}Augmented.java"
-COPY_EXISTING_TEST_FILE="${EXISTING_TEST_FILE%.java}Augmented.java"
+cp "$EXISTING_TEST_FILE_PATH" "${EXISTING_TEST_FILE_PATH%.java}Augmented.java"
+COPY_EXISTING_TEST_FILE_PATH="${EXISTING_TEST_FILE_PATH%.java}Augmented.java"
 
 # call the search-counterexample.py script, which will generate the tests and save them in the output folder
-echo "> Running search-counterexample.py"
-python search-counterexample.py "$CLS" "$CSV_FILE" "$METHOD_NAME"
+echo "> Generate tests using LLM"
+python search-counterexample.py "$SUBJECT_CLASS_PATH" "$SPECS_FILE_PATH" "$METHOD_NAME"
 
-# append the generated tests by llm to the existing test suite
-GENERATED_TEST_FILE="output/test/$(basename "$CLS" .java)/$(basename "$CLS" .java)_${METHOD_NAME}-llm-tests.java"
+# append the generated tests by LLM to the existing test suite
+GENERATED_TEST_FILE="output/test/$(basename "$SUBJECT_CLASS_PATH" .java)/$(basename "$SUBJECT_CLASS_PATH" .java)_${METHOD_NAME}-llm-tests.java"
 echo "> Append the generated tests to the existing test suite"
-python scripts/append_test_suites.py "$COPY_EXISTING_TEST_FILE" "$GENERATED_TEST_FILE"
+python scripts/append_test_suites.py "$COPY_EXISTING_TEST_FILE_PATH" "$GENERATED_TEST_FILE"
