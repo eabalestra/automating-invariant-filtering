95c3204ac3a60eec104d29a22215043c7205516b
eabalestra <agusbalestra0@gmail.com>
2025-02-15 19:59:19 -0300

    Add type hints and UTF-8 encoding to spec_reader functions for improved clarity

--- scripts/spec_reader.py
+++ scripts/spec_reader.py
@@ -1,11 +1,13 @@
 from re import search
 
-def is_inv_line(line):
+
+def is_inv_line(line: str) -> bool:
     return not (search(":::OBJECT", line) or search("==============", line) or search(":::EXIT", line) or search(":::ENTER", line))
 
-def read_and_filter_specs(file_path):
+
+def read_and_filter_specs(file_path: str) -> set:
     specs = []
-    with open(file_path, "r") as file:
+    with open(file_path, "r", encoding="utf-8") as file:
         lines = file.read().splitlines()
         for line in lines:
             if is_inv_line(line):
