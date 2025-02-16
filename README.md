3ebf07009cd7a8d899e7548f008dfbd6c03355df
eabalestra <agusbalestra0@gmail.com>
2025-02-16 20:44:11 -0300

    Update README and script to include test driver file in automatic invariant filtering

--- README.md
+++ README.md
@@ -15,10 +15,10 @@ ollama run test-generation-model (the model will keep running)
 
 ## Usage
 ```bash
-sh run-automatic-invariant-filtering.sh <class_file>.java <specs_file> <method_under_test> <test_suite_file>.java
+sh run-automatic-invariant-filtering.sh <class_file>.java <specs_file> <method_under_test> <test_suite_file>.java <test_driver_file>.java
 ```
 
 ### Example
 ```bash
-sh run-automatic-invariant-filtering.sh examples/simple-examples_getMin/src/main/java/examples/SimpleMethods.java examples/simple-examples_getMin/specs/SimpleMethods-getMin-specfuzzer-1.assertions getMin examples/simple-examples_getMin/src/test/java/testers/SimpleMethodsTester0.java 
+sh run-automatic-invariant-filtering.sh examples/simple-examples_getMin/src/main/java/examples/SimpleMethods.java examples/simple-examples_getMin/specs/SimpleMethods-getMin-specfuzzer-1.assertions getMin examples/simple-examples_getMin/src/test/java/testers/SimpleMethodsTester0.java examples/simple-examples_getMin/src/test/java/testers/SimpleMethodsTesterDriver.java
 ```
--- run-automatic-invariant-filtering.sh
+++ run-automatic-invariant-filtering.sh
@@ -5,10 +5,13 @@ SUBJECT_CLASS_PATH=$1
 SPECS_FILE_PATH=$2
 METHOD_NAME=$3
 EXISTING_TEST_FILE_PATH=$4
+EXISTING_TEST_DRIVER_PATH=$5
 
 # copy the existing test suite
 cp "$EXISTING_TEST_FILE_PATH" "${EXISTING_TEST_FILE_PATH%.java}Augmented.java"
 COPY_EXISTING_TEST_FILE_PATH="${EXISTING_TEST_FILE_PATH%.java}Augmented.java"
+cp "$EXISTING_TEST_DRIVER_PATH" "${EXISTING_TEST_DRIVER_PATH%.java}Augmented.java"
+COPY_EXISTING_TEST_DRIVER_PATH="${EXISTING_TEST_DRIVER_PATH%.java}Augmented.java"
 
 # call the search-counterexample.py script, which will generate the tests and save them in the output folder
 echo "> Generate tests using LLM"
@@ -17,4 +20,4 @@ python search-counterexample.py "$SUBJECT_CLASS_PATH" "$SPECS_FILE_PATH" "$METHO
 # append the generated tests by LLM to the existing test suite
 GENERATED_TEST_FILE="output/test/$(basename "$SUBJECT_CLASS_PATH" .java)/$(basename "$SUBJECT_CLASS_PATH" .java)_${METHOD_NAME}LlmTest.java"
 echo "> Append the generated tests to the existing test suite"
-python scripts/test_appender.py "$COPY_EXISTING_TEST_FILE_PATH" "$GENERATED_TEST_FILE" "$SUBJECT_CLASS_PATH"
+python scripts/test_appender.py "$COPY_EXISTING_TEST_FILE_PATH" "$COPY_EXISTING_TEST_DRIVER_PATH" "$GENERATED_TEST_FILE" "$SUBJECT_CLASS_PATH"
