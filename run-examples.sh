86bef11d5cfbdbb053fbc140a19595a1caafd4b7
eabalestra <agusbalestra0@gmail.com>
2025-02-16 20:44:25 -0300

    Update run-examples.sh to include test driver in invariant filtering and handle augmented files

--- run-examples.sh
+++ run-examples.sh
@@ -9,10 +9,12 @@ echo "==> Running example 1: getMin"
 i=0
 for specs in examples/simple-examples_getMin/specs/*; do
     echo "==> Running example $specs"
-    sh run-automatic-invariant-filtering.sh examples/simple-examples_getMin/src/main/java/examples/SimpleMethods.java $specs getMin examples/simple-examples_getMin/src/test/java/testers/SimpleMethodsTester0.java
+    sh run-automatic-invariant-filtering.sh examples/simple-examples_getMin/src/main/java/examples/SimpleMethods.java $specs getMin examples/simple-examples_getMin/src/test/java/testers/SimpleMethodsTester0.java examples/simple-examples_getMin/src/test/java/testers/SimpleMethodsTesterDriver.java
     augmented_file="examples/simple-examples_getMin/src/test/java/testers/SimpleMethodsTester0Augmented.java"
+    test_driver="examples/simple-examples_getMin/src/test/java/testers/SimpleMethodsTesterDriverAugmented.java"
     if [ -f "$augmented_file" ]; then
         mv "$augmented_file" "output/test/SimpleMethods/SimpleMethodsTester0Augmented-${i}.java"
+        mv "$test_driver" "output/test/SimpleMethods/SimpleMethodsTesterDriverAugmented-${i}.java"
         i=$((i+1))
     else
         echo "File $augmented_file does not exist. Skipping."
