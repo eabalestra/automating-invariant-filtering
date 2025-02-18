f470bffc783b9897474cef2e9d241742669fd0ce
eabalestra <agusbalestra0@gmail.com>
2025-02-18 15:45:14 -0300

    Add support for running MathUtil_clamp examples in run-examples.sh

--- run-examples.sh
+++ run-examples.sh
@@ -4,8 +4,23 @@ if [ -z "$VIRTUAL_ENV" ]; then
     exit 1
 fi
 
-echo "==> Running example 1: getMin"
+echo "==> Running example 2: clamp"
+i=0
+for specs in examples/MathUtil_clamp/specs/*; do
+    echo "==> Running example $specs"
+    sh run-automatic-invariant-filtering.sh examples/MathUtil_clamp/src/main/java/jts/MathUtil.java $specs clamp examples/MathUtil_clamp/src/test/java/testers/MathUtilTester0.java examples/MathUtil_clamp/src/test/java/testers/MathUtilTesterDriver.java
+    augmented_file="examples/MathUtil_clamp/src/test/java/testers/MathUtilTester0Augmented.java"
+    test_driver="examples/MathUtil_clamp/src/test/java/testers/MathUtilTesterDriver.java"
+    if [ -f "$augmented_file" ]; then
+        mv "$augmented_file" "output/test/MathUtil_clamp/MathUtilTester0Augmented-${i}.java"
+        mv "$test_driver" "output/test/MathUtil_clamp/MathUtilTesterDriverAugmented-${i}.java"
+        i=$((i + 1))
+    else
+        echo "File $augmented_file does not exist. Skipping."
+    fi
+done
 
+echo "==> Running example 1: getMin"
 i=0
 for specs in examples/simple-examples_getMin/specs/*; do
     echo "==> Running example $specs"
@@ -15,7 +30,7 @@ for specs in examples/simple-examples_getMin/specs/*; do
     if [ -f "$augmented_file" ]; then
         mv "$augmented_file" "output/test/SimpleMethods/SimpleMethodsTester0Augmented-${i}.java"
         mv "$test_driver" "output/test/SimpleMethods/SimpleMethodsTesterDriverAugmented-${i}.java"
-        i=$((i+1))
+        i=$((i + 1))
     else
         echo "File $augmented_file does not exist. Skipping."
     fi
