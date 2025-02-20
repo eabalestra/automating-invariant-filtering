aa26c00406543c898a5e5d954a84765e56b1da9e
eabalestra <agusbalestra0@gmail.com>
2025-02-20 17:59:12 -0300

    Refactor specification filtering script to improve output handling and add directory creation

--- scripts/remove_not_interest_specs.py
+++ scripts/remove_not_interest_specs.py
@@ -1,7 +1,8 @@
+import os
 import sys
 import pandas as pd
 
-OUTPUT_DIR = 'output/test/'
+AUTOMATIC_IF_OUTPUT_DIR = 'output/test/'
 
 specs_file = sys.argv[1]
 full_qualifier = sys.argv[2]
@@ -10,27 +11,28 @@ method_name = sys.argv[3]
 
 
 def is_of_interest(ppt):
-    if not "EXIT" in ppt:
+    if "ENTER" in ppt:
         return False
     obj_spec = f"{full_qualifier}.{class_name}("
     return (method_name in ppt) or (obj_spec in ppt)
 
 
 input_specifications = pd.read_csv(specs_file)
-input_program_points = input_specifications["ppt"].unique()
-interest_specs = list(filter(is_of_interest, input_program_points))
 
-relevant_specifications = list(set(input_program_points) - set(interest_specs))
+interest_ppts = list(
+    filter(is_of_interest, input_specifications["ppt"].unique()))
 
-output_file = f"{OUTPUT_DIR}{class_name}/{class_name}_{method_name}-interesting.filtered-specs"
+filtered_specs = input_specifications[input_specifications["ppt"].isin(
+    interest_ppts)]
 
-with open(output_file, "w") as file:
-    for spec in interest_specs:
-        complete_spec = input_specifications[input_specifications["ppt"] == spec]
-        complete_spec = complete_spec["invariant"].values[0]
-        file.write(complete_spec + "\n")
+print(f"Total specs: {len(input_specifications['invariant'])}")
+print(f"Filtered specs: {len(filtered_specs)}")
 
-print(f"Total filtered specs: {len(input_specifications)}")
-print(f"Total non-interesting specs: {len(relevant_specifications)}")
-print(f"Total interesting specs: {len(interest_specs)}")
-print(f"Writing specs in: {output_file}")
+output_file_dir = f"{AUTOMATIC_IF_OUTPUT_DIR}"
+output_file = f"{output_file_dir}{class_name}/specs.csv"
+
+# Create the directory if it doesn't exist
+os.makedirs(output_file_dir, exist_ok=True)
+
+filtered_specs.to_csv(output_file, index=False)
+print(f"Filtered specs written in: {output_file}")
