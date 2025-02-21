fe6a2f2c8a3d4fabcb27551f2d46da0dbbb06729
eabalestra <agusbalestra0@gmail.com>
2025-02-21 09:32:58 -0300

    Add code extraction utilities and refactor test processing scripts
    - Introduced `code_extractor.py` with functions to extract package paths, method code, and class names.
    - Refactored `search-counterexample.py` to utilize new extraction functions.
    - Created `test_processor.py` for augmenting test methods and modifying method calls.
    - Updated `test_compiler.py` to improve test compilation logic.
    - Enhanced `run-automatic-invariant-filtering.sh` for better output handling and organization.

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,6 +1,3 @@
-import re
-
-
 def append_test_method_to_file(destination_file: str, test_method: str) -> None:
     try:
         with open(destination_file, 'r+', encoding='utf-8') as df:
@@ -15,42 +12,3 @@ def append_test_method_to_file(destination_file: str, test_method: str) -> None:
             df.truncate()
     except IOError as e:
         print(f'Error processing file {destination_file}: {e}')
-
-
-def extract_package_path(file_path: str) -> str:
-    parts = file_path.split('/java/')
-    if len(parts) > 1:
-        result = parts[1].split('/')
-        return ".".join(result[:-1]).replace('/', '.')
-
-
-def extract_method_code(class_code: str, method_name: str) -> str:
-    pattern = r'^\s*(?:(?:public)|(?:private)|(?:static)|(?:protected)\s+)*.*\s+' + \
-        method_name + r'\s*\(.*\)\s*(?:throws\s+[\w\s,]+)?\s*{'
-
-    match = re.search(pattern, class_code, re.MULTILINE)
-    if not match:
-        return ""
-
-    start_index = match.start()
-    brace_count = 0
-    end_index = start_index
-
-    for i in range(start_index, len(class_code)):
-        if class_code[i] == '{':
-            brace_count += 1
-        elif class_code[i] == '}':
-            brace_count -= 1
-            if brace_count == 0:
-                end_index = i + 1
-                break
-
-    return class_code[start_index:end_index]
-
-
-def extract_class_name(class_code):
-    pattern = r'public\s+class\s+(\w+)'
-    match = re.search(pattern, class_code)
-    if not match:
-        raise ValueError("Class name not found in the provided class code.")
-    return match.group(1)
--- search-counterexample.py
+++ search-counterexample.py
@@ -1,29 +1,24 @@
 import sys
 import os
 from testgen import test_generator
-from scripts import assertion_remover, spec_processor, spec_reader, test_extractor, utils
-
-OUTPUT_DIR = 'output/test/'
+from scripts import assertion_remover, spec_processor, spec_reader, test_extractor, code_extractor
 
 # Load file and arguments
-class_file_path = sys.argv[1]
-specs_file_path = sys.argv[2]
-method_name = sys.argv[3]
+output_dir, class_file, specs_file, method_name = sys.argv[1:5]
 
 # Load class code and method code
-class_code = open(class_file_path, 'r').read()
-method_code = utils.extract_method_code(class_code, method_name)
-class_name = utils.extract_class_name(class_code)
+class_code = open(class_file, 'r').read()
+method_code = code_extractor.extract_method_code(class_code, method_name)
+class_name = code_extractor.extract_class_name(class_code)
 
-likely_valid_specs = spec_reader.read_and_filter_specs(specs_file_path)
-
-# create output dir for class cls
-os.makedirs(os.path.join(OUTPUT_DIR, class_name), exist_ok=True)
+# Read the specs file
+likely_valid_specs = spec_reader.read_and_filter_specs(specs_file)
 
 # create the output file
-test_suite_path = os.path.join(
-    OUTPUT_DIR, class_name, f"{class_name}_{method_name}LlmTest.java")
-with open(test_suite_path, "w") as file:
+output_test_dir = os.path.join(output_dir, "test")
+os.makedirs(output_test_dir, exist_ok=True)
+generated_test_suite = os.path.join(output_test_dir, f"{class_name}_{method_name}LlmTest.java")
+with open(generated_test_suite, "w") as file:
     pass
 
 test_attempts = 1
@@ -38,5 +33,5 @@ for spec in likely_valid_specs:
         final_test = test_extractor.extract_test_with_comments_from_string(
             assertion_free_test)
         # save response to a file in the output directory
-        with open(test_suite_path, 'a') as f:
+        with open(generated_test_suite, 'a') as f:
             f.write(final_test + '\n\n')
