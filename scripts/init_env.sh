3fc8f50f632284b158fb9338dc17f7278a92661d
AgusNolasco <nolasco@dc.exa.unrc.edu.ar>
2025-02-24 19:33:56 -0300

    Update init_env.sh

--- scripts/init_env.sh
+++ scripts/init_env.sh
@@ -1,2 +1,3 @@
 export SUBJECTS_DIR=/home/investigador/nolasco/specfuzzer-tools/GAssert/subjects
 export SPECS_DIR=/home/investigador/nolasco/specfuzzer-exp
+export DAIKONDIR=/home/investigador/nolasco/specfuzzer-tools/daikon-5.8.2
