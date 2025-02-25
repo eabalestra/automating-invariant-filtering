2db565a3f6828c9f81df6b77fc3f9c389252fbbe
eabalestra <agusbalestra0@gmail.com>
2025-02-25 17:21:53 -0300

    Refactor search-mutant.py to improve output handling and streamline mutant generation process

--- search-mutant.py
+++ search-mutant.py
@@ -12,35 +12,27 @@ code = attr_and_method_extractor.extract_class_attributes_and_method(
 class_name = os.path.basename(class_file).replace('.java', '')
 
 # Load test suite
-# TODO: load from a test file all the test one by one (should be sys.argv[3])
-# example = """    @Test
-#     public void llmTest2() throws Throwable {
-#         DataStructures.StackAr stackAr0 = new DataStructures.StackAr(200);
-#         stackAr0.push((java.lang.Object) 0L);
-#         stackAr0.push((java.lang.Object) 0L);
-#         stackAr0.push((java.lang.Object) 0L);
-#         stackAr0.pop();
-#     }"""
 test_suite = test_extractor.extract_tests_from_file(sys.argv[3])
 
 # Read the specs file
-# likely_valid_specs = spec_reader.read_and_filter_specs(specs_file)
-# TODO: use the script to read all the non mutant killing specs
-# non_mutant_killing_specs = [sys.argv[4]]
 non_mutant_killing_specs = spec_reader.read_and_filter_specs(sys.argv[4])
 
 # create the output file
-# output_test_dir = os.path.join(output_dir, "test")
-# os.makedirs(output_test_dir, exist_ok=True)
-# generated_test_suite = os.path.join(output_test_dir, f"{class_name}_{method_name}LlmTest.java")
+output_dir = sys.argv[5]
+output_mutant_dir = os.path.join(output_dir, "llm")
+os.makedirs(output_mutant_dir, exist_ok=True)
+mutation_file = os.path.join(
+    output_mutant_dir, f"{class_name}_{method_name}LlmGeneratedMutants.txt")
 
 mutation_attempts = 1
-for spec in non_mutant_killing_specs:
-    print(f"Generating mutant for spec: {spec}")
-    spec = spec_processor.update_specification_variables(spec, class_name)
-    for test in test_suite:
-        for i in range(mutation_attempts):
-            mutation = mutant_generator.generate_mutant(code, test, spec)
-            print(mutation)
-            # save response to a file in the output directory
-            # f.write(final_test + "\n")
+with open(mutation_file, 'a') as f:
+    for spec in non_mutant_killing_specs:
+        print(f"Original spec: {spec}")
+        spec = spec_processor.update_specification_variables(spec, class_name)
+        print(f"calling llm for spec: {spec}")
+        for test in test_suite:
+            for i in range(mutation_attempts):
+                mutation = mutant_generator.generate_mutant(code, test, spec)
+                print(mutation)
+                # save response to a file in the output directory
+                f.write(mutation + '\n')
