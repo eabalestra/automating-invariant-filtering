d4c0b83b47e06f74f51cb41da17aa70fb1d49845
eabalestra <agusbalestra0@gmail.com>
2025-02-25 08:22:43 -0300

    Add script for mutation-based invariant filtering and update compute-non-mutants-killing-specs.py for output organization

---
+++ run-mutant-invariant-filtering.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Arguments
+subject_name="$1"
+target_class_fqname="$2"
+method_name="$3"
+test_suite_name="$4"
+
+# Find the files
+class_name="${target_class_fqname##*.}"
+class_path=$(find "$SUBJECTS_DIR/$subject_name/src/main/java" -type f -name "$class_name".java)
+assertions_file="$SPECS_DIR/$subject_name/output/$class_name-$method_name-specfuzzer-1.assertions"
+invs_by_mutants="$SPECS_DIR/$subject_name/output/$class_name-$method_name-specfuzzer-1-invs-by-mutants.csv"
+test_suite=$(find "$SUBJECTS_DIR/$subject_name/src/test/java" -type f -name "$test_suite_name".java)
+
+assertions_file_name=$(basename "${assertions_file%.*}")
+
+echo "=> Running mutation-based invariant filtering"
+echo "> Class: $class_name"
+echo "> Method: $method_name"
+echo "> Test suite: $test_suite_name"
+
+# Create the output directory
+automating_if_subject_dir="output/${class_name}_${method_name}"
+mutants_dir="$automating_if_subject_dir/mutants"
+mkdir -p "$mutants_dir"
+
+# Get the non-mutant killing assertions
+python scripts/compute-non-mutants-killing-specs.py "$assertions_file" "$invs_by_mutants" "$class_name" "$method_name"
+non_mutant_killing_assertions_file="$mutants_dir/${assertions_file_name}-non-mutant-killing.assertions"
+
+# Generate mutants using LLM
+python search-mutant.py "$class_path" "$method_name" "$test_suite" "$non_mutant_killing_assertions_file"
--- scripts/compute-non-mutants-killing-specs.py
+++ scripts/compute-non-mutants-killing-specs.py
@@ -1,7 +1,7 @@
+import os
 import sys
 import pandas as pd
 from re import search
-#from spec_processor import *
 
 specs_file = sys.argv[1]
 specs_and_mutants_file = sys.argv[2]
@@ -13,6 +13,7 @@ print(specs_and_mutants_file)
 def is_inv_line(line):
     return not (search(":::OBJECT", line) or search("==============", line) or search(":::EXIT", line) or search(":::ENTER", line))
 
+
 specs = []
 with open(specs_file, "r") as file:
     lines = file.read().splitlines()
@@ -20,12 +21,12 @@ with open(specs_file, "r") as file:
         if is_inv_line(line):
             specs.append(line)
     specs = set(specs)
-    #specs.sort()
+
 
 def is_of_interest(spec):
     curr_spec = spec
     if (curr_spec.startswith("FuzzedInvariant:")):
-        curr_spec = curr_spec.replace("FuzzedInvariant:","")
+        curr_spec = curr_spec.replace("FuzzedInvariant:", "")
     if ("daikon.Quant.memberOf" in spec):
         return True
     for inv in specs:
@@ -33,10 +34,10 @@ def is_of_interest(spec):
             return True
     return False
 
+
 specs_and_mutants_df = pd.read_csv(specs_and_mutants_file)
 mutant_killing_specs = specs_and_mutants_df["invariant"].unique()
 mutant_killing_specs = list(filter(is_of_interest, mutant_killing_specs))
-#mutant_killing_specs.sort()
 
 print(f"Total specs: {len(specs)}")
 print(f"Total mutant-killing specs: {len(mutant_killing_specs)}")
@@ -47,17 +48,18 @@ print(f"Total non-mutant-killing specs: {len(non_mutant_killing_specs)}")
 
 w = set(mutant_killing_specs) - specs
 if w != set():
-    print(f"specs in mutant-killing specs that do not belong to the set of total specs: {w}")
+    print(
+        f"specs in mutant-killing specs that do not belong to the set of total specs: {w}")
     print("TODO: Where this specs come from?")
 
 subject_name = specs_file.split("/")[-1].split(".")[0]
 
-output_dir = "output/non-mutant-killing-assertions/"
-output_file = output_dir + subject_name + ".assertions"
+class_name = sys.argv[3]
+method_name = sys.argv[4]
+output_dir = f"output/{class_name}_{method_name}/mutants/"
+output_file = output_dir + subject_name + "-non-mutant-killing.assertions"
 print(f"Writing specs in: {output_file}")
 
 with open(output_file, "w") as file:
     for spec in non_mutant_killing_specs:
-        #spec = replace_spec_variables(spec)
-        #spec = strip_outer_parentheses(spec)
         file.write(spec + "\n")
