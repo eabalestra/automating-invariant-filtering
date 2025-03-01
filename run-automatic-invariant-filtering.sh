b211eb6d321542137c5ee1ae1a83c78e1ea8d729
eabalestra <agusbalestra0@gmail.com>
2025-03-01 17:28:22 -0300

    Refactor run-automatic-invariant-filtering.sh to improve output logging and remove unnecessary echo statements

--- run-automatic-invariant-filtering.sh
+++ run-automatic-invariant-filtering.sh
@@ -6,17 +6,17 @@ subject_name=$1
 class_name=$2
 method_name=$3
 class_path=$(find "$SUBJECTS_DIR/$subject_name/src/main/java" -type f -name "$class_name".java)
-echo "$class_path"
 spec_file="$SPECS_DIR/$subject_name/output/$class_name-$method_name-specfuzzer-1.assertions"
-echo "$spec_file"
 test_suite_name=$4
 test_suite=$(find "$SUBJECTS_DIR/$subject_name/src/test/java" -type f -name "$test_suite_name".java)
-echo "$test_suite"
 test_driver_name=$5
 test_driver=$(find "$SUBJECTS_DIR/$subject_name/src/test/java" -type f -name "$test_driver_name".java)
-echo "$test_driver"
 
 echo "==> Running automatic invariant filtering"
+echo "Class: $(basename "$class_path")"
+echo "Spec file: $(basename "$spec_file")"
+echo "Test suite: $(basename "$test_suite")"
+echo "Test driver: $(basename "$test_driver")"
 
 # create the output folder
 output_dir="output/${class_name}_${method_name}"
@@ -26,34 +26,32 @@ log_file="$output_dir/${class_name}_${method_name}.log"
 
 # copy the existing test suite
 augmented_test_suite="${test_suite%.java}Augmented.java"
-echo "$augmented_test_suite"
 cp "$test_suite" "$augmented_test_suite"
 augmented_test_driver="${test_driver%.java}Augmented.java"
-echo "$augmented_test_driver"
 cp "$test_driver" "$augmented_test_driver"
 
 # generate tests using LLM
 echo "> Generate tests using LLM"
-python search-counterexample.py "$output_dir" "$class_path" "$spec_file" "$method_name" >>"$log_file"
+python search-counterexample.py "$output_dir" "$class_path" "$spec_file" "$method_name"
 tests_output_dir="$output_dir/test"
 llm_generated_test_suite="$tests_output_dir/${class_name}_${method_name}LlmTest.java"
 
 echo "> Prepare destination for the generated tests"
-python scripts/prepare_destination_test_files.py "$augmented_test_suite" "$augmented_test_driver" >>"$log_file"
+python scripts/prepare_destination_test_files.py "$augmented_test_suite" "$augmented_test_driver"
 
 # fix the generated tests
 echo "> Fix the generated tests"
-python scripts/fix_llm_tests.py "$tests_output_dir" "$llm_generated_test_suite" "$class_path" "$method_name" >>"$log_file"
+python scripts/fix_llm_tests.py "$tests_output_dir" "$llm_generated_test_suite" "$class_path" "$method_name"
 llm_fixed_test_suite="$tests_output_dir/${class_name}_${method_name}LlmFixedTest.java"
 
 # get the compilable test suite
 echo "> Compile the test suite"
-python scripts/discard_uncompilable_llm_tests.py "$tests_output_dir" "$augmented_test_suite" "$class_path" "$llm_fixed_test_suite" "$method_name" >>"$log_file"
+python scripts/discard_uncompilable_llm_tests.py "$tests_output_dir" "$augmented_test_suite" "$class_path" "$llm_fixed_test_suite" "$method_name"
 llm_compilable_test_suite="$tests_output_dir/${class_name}_${method_name}LlmCompilableTest.java"
 
 # append the generated tests by LLM to the existing test suite
 echo "> Append the generated tests to the existing test suite"
-python scripts/append_llm_tests.py "$augmented_test_suite" "$augmented_test_driver" "$llm_compilable_test_suite" "$class_path" >>"$log_file"
+python scripts/append_llm_tests.py "$augmented_test_suite" "$augmented_test_driver" "$llm_compilable_test_suite" "$class_path"
 
 echo "> Done"
 echo "Output is saved in $output_dir"
