12721fae61ca765188bff15a36659ce36e3da7bf
eabalestra <agusbalestra0@gmail.com>
2025-03-04 17:56:02 -0300

    Refactor prepare_destination_test_files.py to accept a class name parameter for renaming

--- scripts/prepare_destination_test_files.py
+++ scripts/prepare_destination_test_files.py
@@ -13,17 +13,17 @@ def add_augmented_to_constructor_subject(file_path: str, subject_name: str) -> N
     write_file(file_path, new_content, 'w')
 
 
-def rename_classes_in_file(file_path: str) -> None:
+def rename_classes_in_file(file_path: str, name: str) -> None:
     content = read_file(file_path, 'r')
     pattern = r'(public\s+class\s+)(\w+)(\s*\{)'
-    replacement = r'\1\2Augmented\3'
+    replacement = fr'\1\2{name}\3'
     new_content = re.sub(pattern, replacement, content)
     write_file(file_path, new_content, 'w')
 
 
-def prepare_destination_files(destination_test_driver: str, destination_test_suite: str) -> None:
-    rename_classes_in_file(destination_test_suite)
-    rename_classes_in_file(destination_test_driver)
+def prepare_destination_files(destination_test_driver: str, destination_test_suite: str, name: str) -> None:
+    rename_classes_in_file(destination_test_suite, name)
+    rename_classes_in_file(destination_test_driver, name)
     tester_class_name = os.path.basename(
         destination_test_suite).replace('Augmented.java', '')
     add_augmented_to_constructor_subject(
@@ -32,9 +32,10 @@ def prepare_destination_files(destination_test_driver: str, destination_test_sui
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        print("Usage: python prepare_destination_test_files.py <destination_suite>.java <destination_driver>.java")
+        print("Usage: python prepare_destination_test_files.py <destination_suite>.java <destination_driver>.java <name>")
         sys.exit(1)
 
-    destination_test_suite, destination_test_driver = sys.argv[1:3]
+    destination_test_suite, destination_test_driver, name = sys.argv[1:4]
 
-    prepare_destination_files(destination_test_driver, destination_test_suite)
+    prepare_destination_files(destination_test_driver,
+                              destination_test_suite, name)
