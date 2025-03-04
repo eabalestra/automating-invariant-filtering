88407408bcddb52fff74dee418040821b6ff6716
eabalestra <agusbalestra0@gmail.com>
2025-03-04 20:39:21 -0300

    Refactor extract_tests_and_mutants.py to fix the extracted tests

--- scripts/extract_tests_and_mutants.py
+++ scripts/extract_tests_and_mutants.py
@@ -3,13 +3,17 @@ import re
 
 import pandas as pd
 
+from fix_llm_tests import add_test_signatures, add_throws_declaration, rename_test_methods, replace_class_references
+
 tests_and_mutants_file = sys.argv[1]
 output_mutation = sys.argv[2]
+class_file = sys.argv[3]
 
 reading_test = False
 reading_mutant = False
 
-tests_and_mutants = []
+tests = []
+mutants = []
 
 test = ""
 mutant = ""
@@ -34,15 +38,25 @@ with open(tests_and_mutants_file, 'r') as f:
                 mutant += line
             else:
                 reading_mutant = False
-                tests_and_mutants.append((test, mutant))
+                if test != "":
+                    tests.append(test)
+                mutants.append(mutant)
                 mutant = ""
                 test = ""
 
-specs_and_mutants_df = pd.DataFrame(
+
+tests = rename_test_methods(tests, 'llmMutationTest')
+tests = replace_class_references(tests, class_file)
+tests = add_throws_declaration(tests)
+tests = add_test_signatures(tests)
+
+tests_and_mutants = list(zip(tests, mutants))
+
+tests_with_mutants_df = pd.DataFrame(
     tests_and_mutants, columns=["test", "mutant"])
 
-specs_and_mutants_df.to_csv("mutant_tests.csv", index=False)
+tests_with_mutants_df.to_csv("mutant_tests.csv", index=False)
 
 with open(output_mutation, 'w') as mutants_file:
-    for (t, m) in tests_and_mutants:
+    for m in tests_with_mutants_df["mutant"].unique():
         mutants_file.write(m)
