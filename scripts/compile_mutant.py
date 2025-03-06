c55f88ce56ebecf00a1654abfab542191ba62f59
eabalestra <agusbalestra0@gmail.com>
2025-03-06 18:05:59 -0300

    Add compile_mutant.py script to build mutants using Gradle

---
+++ scripts/compile_mutant.py
@@ -0,0 +1,25 @@
+import os
+import subprocess
+import sys
+
+subject_build_dir = sys.argv[1]
+
+current_dir = os.getcwd()
+
+os.chdir(subject_build_dir)
+
+result = subprocess.run(
+    ['./gradlew', '-q', '-Dskip.tests', 'jar'],
+    stdin=subprocess.DEVNULL,
+    stdout=subprocess.DEVNULL,
+    stderr=subprocess.DEVNULL
+)
+
+build_status = result.returncode
+
+os.chdir(current_dir)
+
+if build_status != 0:
+    sys.exit(1)
+else:
+    sys.exit(0)
