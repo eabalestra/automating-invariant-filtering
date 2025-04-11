aaf461f5ac35a71a6338553457d0e057a4764c0d
AgusNolasco <nolasco@dc.exa.unrc.edu.ar>
2025-04-10 22:12:22 -0300

    Add timestamp log

--- search-counterexample.py
+++ search-counterexample.py
@@ -19,14 +19,29 @@ output_test_dir = os.path.join(output_dir, "test")
 os.makedirs(output_test_dir, exist_ok=True)
 generated_test_suite = os.path.join(output_test_dir, f"{class_name}_{method_name}LlmTest.java")
 
+# timestamp log
+llm_test_timestamp = os.path.join(output_test_dir, "timestamps.log")
+log = open(llm_test_timestamp, "w")
+
 test_attempts = 1
+total_time = 0.0
 with open(generated_test_suite, 'a') as f:
     for spec in likely_valid_specs:
         for i in range(test_attempts):
             print(f"Generating test for spec: {spec}")
             processed_spec = spec_processor.update_specification_variables(spec, class_name)
+
+            start_time = time.time()
             generated_test = test_generator.generate_test(class_name, class_code, method_code, processed_spec)
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            total_time += elapsed_time
+            log.write(f"Time taken for LLM response for {spec}: {elapsed_time:.4f} sec\n")
+
             assertion_free_test = assertion_remover.strip_assertions_from_test_code(generated_test)
             final_test = test_extractor.extract_test_with_comments_from_string(assertion_free_test)
             # save response to a file in the output directory
             f.write(final_test + "\n")
+
+log.write(f"\nTotal LLM time for {class_name}_{method_name}: {total_time:.4f} seconds\n")
+log.close()
