7422c086039f1bb8338aa24c9636bf4f083a48c7
eabalestra <agusbalestra0@gmail.com>
2025-05-17 15:11:15 -0300

    Remove commented-out code for subject recompilation in second round validation script

--- run-second-round-validation.sh
+++ run-second-round-validation.sh
@@ -80,10 +80,6 @@ fi
 
 # Recompile the subject
 echo "> Recompiling subject: $target_class" | tee -a "$log_file"
-#current_dir=$(pwd)
-#cd "$subject_sources" || exit
-#./gradlew -q -Dskip.tests jar
-#cd "$current_dir" || exit
 python3 scripts/compile_subject.py "$subject_sources"
 
 # Perform the Dynamic Comparability Analysis
