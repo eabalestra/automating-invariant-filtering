02ee452244f5cb91ffed89d49cd4aee8057c95ac
eabalestra <agusbalestra0@gmail.com>
2025-07-11 15:44:41 -0300

    Remove obsolete example and model run scripts

--- scripts/shell/run-model.sh
+++
@@ -1,6 +0,0 @@
-echo "> Deleting old model"
-ollama rm test-generation-model
-echo "> Creating new model with Modelfile"
-ollama create test-generation-model -f testgen/Modelfile
-echo "> Running model"
-ollama run test-generation-model &
