56007cff510b38adb0ec3f13472e60092e42048e
eabalestra <agusbalestra0@gmail.com>
2025-07-11 14:58:36 -0300

    Add requirements.txt and setup_env.sh for environment configuration

---
+++ config/setup_env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Configurations for the environment setup
+export SPECS_DIR=
+export OPENAI_API_KEY=
+export API_KEY_HUGGINGFACE=
+export DAIKONDIR=
+export SUBJECTS_DIR=
+export GOOGLE_API_KEY=
