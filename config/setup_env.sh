01df0f4dae1a2c2cf91b59936d0f9149f3fd86ff
eabalestra <agusbalestra0@gmail.com>
2025-07-22 16:29:10 -0300

    Refactor environment variable setup in setup_env.sh for clarity and organization

--- config/setup_env.sh
+++ config/setup_env.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 # Configurations for the environment setup
-export SPECS_DIR=
-export OPENAI_API_KEY=
-export API_KEY_HUGGINGFACE=
-export DAIKONDIR=
-export SUBJECTS_DIR=
-export GOOGLE_API_KEY=
+
+# Environment variables for directories
+export SPECS_DIR=path/to/specfuzzer-subject-results
+export DAIKONDIR=path/to/daikon-5.8.2
+export SUBJECTS_DIR=path/to/GAssert/subjects
+
+# Environment variables for API keys
+export OPENAI_API_KEY=your_openai_api_key
+export GOOGLE_API_KEY=your_google_api_key
+export API_KEY_HUGGINGFACE=your_huggingface_api_key
