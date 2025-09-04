6b4330eb79d7477225af499cdbf22b887ef578b9
eabalestra <agusbalestra0@gmail.com>
2025-09-04 09:34:42 -0300

    Refactor filter_invariants_of_interest.py for improved readability and correctness in class name extraction

--- scripts/filter_invariants_of_interest.py
+++ scripts/filter_invariants_of_interest.py
@@ -2,14 +2,16 @@ import os
 import sys
 import pandas as pd
 
-AUTOMATIC_IF_OUTPUT_DIR = 'output'
+AUTOMATIC_IF_OUTPUT_DIR = "output"
 
 specs_file = sys.argv[1]
 full_qualifier = sys.argv[2]
-class_name = full_qualifier.split(".")[1]
+class_name = full_qualifier.split(".")[-1]
 method_name = sys.argv[3]
 
 output_directory = f"{AUTOMATIC_IF_OUTPUT_DIR}/{class_name}_{method_name}/specs"
+
+os.makedirs(output_directory, exist_ok=True)
 
 
 def is_of_interest(ppt):
@@ -23,7 +25,9 @@ input_specifications = pd.read_csv(specs_file)
 
 interest_ppts = list(filter(is_of_interest, input_specifications["ppt"].unique()))
 
-interest_filtered_specs = input_specifications[input_specifications["ppt"].isin(interest_ppts)]
+interest_filtered_specs = input_specifications[
+    input_specifications["ppt"].isin(interest_ppts)
+]
 
 output_file = f"{output_directory}/interest-specs.csv"
 
